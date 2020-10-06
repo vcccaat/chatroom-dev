e8a83785e824ad46c8449dca9fa746f3ff95a648
vcccaat <yzhuba@connect.ust.hk>
2020-10-06 14:19:34 +0800

    update gh-pages

---
+++ docs/precache-manifest.b5adcb97d86054f1ac51b6b4fa390b1c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "60f4e45869c7a0599021c8945e7e4ff3",
+    "url": "/chatroom/index.html"
+  },
+  {
+    "revision": "29f55d2d09df741b55a2",
+    "url": "/chatroom/static/css/main.184d6db6.chunk.css"
+  },
+  {
+    "revision": "4f7afa9ec87e4cd79447",
+    "url": "/chatroom/static/js/2.a42417f6.chunk.js"
+  },
+  {
+    "revision": "5f8be6edcb631ea3c834b8459dc2c264",
+    "url": "/chatroom/static/js/2.a42417f6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "29f55d2d09df741b55a2",
+    "url": "/chatroom/static/js/main.aba313b4.chunk.js"
+  },
+  {
+    "revision": "7555aee25a7eccec95c5",
+    "url": "/chatroom/static/js/runtime-main.a0ec63dd.js"
+  }
+]);
--- docs/service-worker.js
+++ docs/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "./precache-manifest.f474914dd08895babf786678fae01c69.js"
+  "/chatroom/precache-manifest.b5adcb97d86054f1ac51b6b4fa390b1c.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("./index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/chatroom/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
